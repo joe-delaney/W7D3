85862cc01896a168cd64010a21b20f527e50cfe9
App Academy Student <email@email.com>
2022-03-16 15:47:00 -0400

    break

--- goals_app/app/controllers/users_controller.rb
+++ goals_app/app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new, status: 422
+    end
+  end
+
+  private
+  def user_params 
+    params.require(:user).permit(:username, :password)
+  end
 end
--- goals_app/spec/controllers/users_controller_spec.rb
+++ goals_app/spec/controllers/users_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe UsersController, type: :controller do
 
   describe "GET #show" do
     it "renders the user's show page" do 
-      get :show
+      get :show, params: {id: User.last.id}
       expect(response).to render_template(:show)
     end
   end
@@ -20,5 +20,36 @@ RSpec.describe UsersController, type: :controller do
       get :new
       expect(response).to render_template(:new)
     end 
+  end
+
+  describe "POST #create" do
+    context 'with valid params' do 
+      before(:each) do 
+          post :create, params: { user: {username: 'Harry Potter5', password: "password"}}
+      end 
+
+      it 'creates the user' do 
+          expect(User.last.username).to eq('Harry Potter5')
+      end 
+
+      it 'redirects to user show page' do 
+          expect(response).to redirect_to(user_url(User.last))
+      end    
+    end 
+
+    context 'with invalid params' do 
+      before(:each) do 
+          post :create, params: { user: {not_an_attribute: ''}}
+      end 
+
+      it 'renders the new template' do 
+          expect(response).to render_template(:new)
+          expect(response).to have_http_status(422)
+      end 
+
+      it 'adds errors to flash' do 
+          expect(flash[:errors]).to be_present
+      end 
+    end
   end
 end
